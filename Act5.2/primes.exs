f938993954c9e05da96c43091b41528d404d7a62
danielsanchezsa <danielsanchez.2000@hotmail.com>
2022-06-10 18:09:34 -0500

    Terminando Act.5.2

--- Act5.2/primes.exs
+++ Act5.2/primes.exs
@@ -1,22 +1,19 @@
 # The sum of the primes below 5 000 000 is 838,596,693,108.
 # Find the sum of all the primes below five million.
 
-
 defmodule Hw.Primes do
-
-  #Falta definir el timer
-  def timer(funtion)do
-    fuction
-
+  def timer(function) do
+    function
+    |> :timer.tc()
+    |> elem(0)
+    |> Kernel./(1_000_000)
   end
 
-
-
   def sum_primes do
-    Stream.unfold {}, fn(accu) ->
+    Stream.unfold({}, fn accu ->
       prime = next_prime(accu)
-      { prime, accu |> :erlang.append_element(prime) }
-    end
+      {prime, accu |> :erlang.append_element(prime)}
+    end)
   end
 
   defp next_prime({}), do: 2
@@ -37,21 +34,68 @@ defmodule Hw.Primes do
 
   defp prime?(num, prev_primes, i) do
     check = prev_primes |> elem(i)
+
     cond do
-      num < check*check     -> true
-      rem(num, check) == 0  -> false
-      true                  -> prime?(num, prev_primes, i + 1)
+      num < check * check -> true
+      rem(num, check) == 0 -> false
+      true -> prime?(num, prev_primes, i + 1)
     end
   end
-  Primes.sum_primes
-  |> Enum.take_while(fn(n) -> n < 5_000_000 end)
-  |> Enum.sum
-  |> IO.inspect
 
-  # Implementar suma de forma paralela
 
-  #recibe el limite de numeros primos y el numero de threads a lanzar
+
+
+
+
+
+  def sum_primes_config(range, init) do
+IO.puts("range: #{range}")
+IO.puts("init: #{init}")
+    Hw.Primes.sum_primes
+    |> Enum.take_while(fn n -> n < range end)
+    |> Enum.sum()
+    |> IO.inspect()
+  end
+
+
+
+
+  def sum_primes_parallel(limit, threads \\ System.schedulers()) do
+    range = div(limit, threads)
+
+    1..threads
+    |> Enum.map(&Task.async(fn -> sum_primes_config(&1 * range, 1 + (&1 - 1) * range) end))
+    |> Enum.map(&Task.await(&1))
+    |> Enum.sum()
+
+  end
+
+
+
+
+
+
+
+  def comparative(limit, threads) do
+
+    secuencial = Hw.Primes.sum_primes()|> Enum.take_while(fn n -> n < limit end)|> Enum.sum()
+    parallel = Hw.Primes.sum_primes_parallel(limit, threads)
+
+
+    timerSec = timer(fn ->  Hw.Primes.sum_primes()|> Enum.take_while(fn n -> n < limit end)|> Enum.sum() end)
+    # timerPar =  timer(fn ->Hw.Primes.sum_primes_parallel(limit, threads)end)
+
+    secText = "Secuencial: #{secuencial} time: #{timerSec}"
+    # parText = "Parallel: #{parallel} time: #{timerPar}"
+
+    # speedUp = "SpeedUp comparing Secuencial over parallel: #{timerSec/timerPar }"
+
+
+     IO.puts(secText)
+    # IO.puts(parText)
+    # IO.puts(speedUp)
+
+  end
 end
 
-# Prime test - 1 argument
-#IO.puts(Hw.Primes.sum_primes())
+Hw.Primes.comparative(5000,4)
